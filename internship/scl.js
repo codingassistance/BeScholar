6a202ff51f6c071e648fbd55b04f7b3e64d2a927
Vaishnavi Shet <116947235+codingassistance@users.noreply.github.com>
2023-02-13 12:49:14 +0530

    Delete internship directory

--- internship/scl.js
+++
@@ -1,9 +0,0 @@
-function search(e) {
-	let searched = document.getElementById("search").value.trim();
-  if (searched !== "") {
-  	let text = document.getElementById("text").innerHTML;
-  	let re = new RegExp(searched,"g"); // search for all instances
-		let newText = text.replace(re, `<mark>${searched}</mark>`);
-		document.getElementById("text").innerHTML = newText;
-  }
-}
